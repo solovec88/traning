1904b85574dda0ca0e951693bb23f30c91c15a1b
Solovec <vertuk@mail.ru>
2020-04-06 22:26:52 +0300

    first commit

---
+++ 1/first_1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello there.\n";
+    cout << "Here is 5: " << 5 << "\n";
+    cout <<
+            endl;
+    cout << "hereis averu big number:\t"  << 70000 << endl;
+    cout << "here  is the sum of 8 and 5:\t" << 8+5 << endl;
+    cout << "here's a fraction:\t\t" <<(float) 5/8 << endl;
+    cout << "and vary very big number:\t";
+    cout << (double) 7000 * 7000 <<
+            endl;
+    cout << "don't forget to replace Jess Liberty with your name... \n";
+    cout << "Jesse Liberty is a C++ programmer!\n";
+    return 0;
+
+
+}
