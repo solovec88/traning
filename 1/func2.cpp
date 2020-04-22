8ca80b4fd0a479ddd8dc74110421f58f18c242ec
Solovec <vertuk@mail.ru>
2020-04-22 22:48:14 +0300

    merge33

--- 1/func2.cpp
+++ 1/func2.cpp
@@ -18,3 +18,12 @@ int main(){
     return 0;
 }
 
+cout << "\n Here's the temperature in Celsius:";
+    cout << TempCel << endl;
+    return 0;
+
+    float Convert(float TempFer){
+    float TempCel;
+    TempCel = ((TempFer- 32) * 5) / 9;
+    return TempCel;
+}
