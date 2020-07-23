223fd111618089108e02dba62ed47fa328ec39ac
solovec88 <vertuk@mail.ru>
2020-07-23 22:36:44 +0000

    folder_03

---
+++ folder_03/range.py
@@ -0,0 +1,11 @@
+a = range(5, 16)
+print(a) 
+print('______')
+b = list(range(1, 50, 10))
+print(b) 
+print('______')
+for number in range(5):
+    print(number)
+print('______')
+for number in [0, 1, 2, 3, 4]:
+    print(number)   
---
+++ folder_03/range2.py
@@ -0,0 +1,11 @@
+a_dict = {1:"one", 2:"two", 3:"three"}
+keys = a_dict.keys()
+ 
+keys = sorted(keys)
+for key in keys:
+    print(key)    
+
+
+for number in range(10):
+    if number % 3 == 0:
+        print(number)
